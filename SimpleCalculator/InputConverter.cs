6c1485bbc13c3abdd6e6e4b1b0a0bca953767486
Crystal Reimche <36277207+CrystalReimche@users.noreply.github.com>
2022-01-08 17:29:27 -0600

    Created ConvertInputToNum class

---
+++ SimpleCalculator/InputConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SimpleCalculator
+{
+    public class InputConverter
+    {
+        public double ConvertInputToNum(string argTextInput) 
+        {
+            double convertedNumber;
+
+            // check to see if user input sucessfully converted to number
+            if (!double.TryParse(argTextInput, out convertedNumber))
+            {
+                throw new ArgumentException("Expected a numeric value");
+            }
+            return convertedNumber;
+        }
+    }
+}
