47b9d3f336d24e67afdffe3aaf437dcbac966269
Crystal Reimche <36277207+CrystalReimche@users.noreply.github.com>
2022-01-08 17:36:03 -0600

    added CalculatorEngine class

---
+++ SimpleCalculator/CalculatorEngine.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SimpleCalculator
+{
+    public class CalculatorEngine
+    {
+        public double Calculate(string operation, double firstNumber, double secondNumber)
+        {
+            var result = operation.ToLower() 
+                
+            switch
+            {
+                "add" or "+" => firstNumber + secondNumber,
+                "subtract" or "-" => firstNumber - secondNumber,
+                "multiply" or "*" => firstNumber * secondNumber,
+                "divide" or "/" => firstNumber / secondNumber,
+                _ => throw new InvalidOperationException("Specified operation is not recognized."),
+            };
+
+            return result;
+        }
+    }
+}
