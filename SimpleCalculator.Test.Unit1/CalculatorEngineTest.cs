cddecaf34dcea382b1adef52f04769ce679731bf
Crystal Reimche <36277207+CrystalReimche@users.noreply.github.com>
2022-01-08 21:08:46 -0600

    Added Unit Testing for Input Converter

--- SimpleCalculator.Test.Unit1/CalculatorEngineTest.cs
+++ SimpleCalculator.Test.Unit1/CalculatorEngineTest.cs
@@ -8,16 +8,17 @@ using System.Threading.Tasks;
 
 namespace SimpleCalculator.Test.Unit
 {
+    [TestClass()]
     public class CalculatorEngineTest
     {
-     
+        private readonly CalculatorEngine _calculatorEngine = new CalculatorEngine();
 
         [TestMethod()]
         public void AddTwoNumbersAndReturnsValidResultForNonSymbolOperation()
         {
             int number1 = 1;
             int number2 = 2;
-            double result = CalculatorEngine.Calculate("add", number1, number2);
+            double result = _calculatorEngine.Calculate("add", number1, number2);
 
             // takes in an expected result and an actual result.  If they are equal, it will pass.
             Assert.AreEqual(3, result);
@@ -28,10 +29,12 @@ namespace SimpleCalculator.Test.Unit
         {
             int number1 = 1;
             int number2 = 2;
-            double result = CalculatorEngine.Calculate("+", number1, number2);
+            double result = _calculatorEngine.Calculate("+", number1, number2);
 
             // takes in an expected result and an actual result.  If they are equal, it will pass.
             Assert.AreEqual(3, result);
         }
+
+        
     }
 }
---
+++ SimpleCalculator.Test.Unit1/InputConverterTest.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleCalculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator.Test.Unit
+{
+    [TestClass()]
+    public class InputConverterTest
+    {
+        private readonly InputConverter _inputConverter = new InputConverter();
+
+        [TestMethod()]
+        public void ConvertValidStringInputIntoDouble()
+        {
+            string inputNumber = "5";
+            double convertedNumber = _inputConverter.ConvertInputToNum(inputNumber);
+            Assert.AreEqual(5, convertedNumber);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentException))]
+        public void FailsToConvertValidStringInputIntoDouble()
+        {
+            string inputNumber = "*";
+            double convertedNumber = _inputConverter.ConvertInputToNum(inputNumber);
+            Assert.AreEqual(5, convertedNumber);
+        }
+    }
+}
